2883ab002ceb9e3ee1f54cbb962105b0679a5aef
mrforgotten <harishnarote7@gmail.com>
2025-07-18 23:02:07 +0530

    Add jest-mock-extended for improved testing and remove obsolete jest.config.js

--- tests/Sample.test.ts
+++ tests/Sample.test.ts
@@ -1,3 +1,36 @@
-test("Initial test", () => {
-   expect(2).toEqual(2);
+
+import { prismaMock } from '../singleton';
+import { Project } from '../src/generated/prisma';
+import { createProject } from '../src/services/project.service';
+
+describe('createProject', () => {
+  it('should create a new project and return it', async () => {
+    // input for service 
+    const projectInput = {
+      name: 'EventHub',
+      imageUrl: 'https://example.com/image.png',
+      githubUrl: 'https://github.com/example/eventhub',
+      deployUrl: 'https://eventhub.example.com',
+    };
+    //expected output from service 
+    const mockCreatedProject: Project = {
+      id: 1,
+      name: 'EventHub',
+      imageUrl: 'https://example.com/image.png',
+      githubUrl: 'https://github.com/example/eventhub',
+      deployUrl: 'https://eventhub.example.com',
+    };
+    //this stops the call from doing to database and returns the value as resolved promise
+    prismaMock.project.create.mockResolvedValue(mockCreatedProject);
+
+    // gets the result of the service 
+    const result = await createProject(projectInput);
+
+
+    expect(prismaMock.project.create).toHaveBeenCalledWith({
+      data: projectInput,
+    });
+    expect(result).toEqual(mockCreatedProject);
+  });
 });
+
