16d8edf3617639b27d84b73282d7cda6ff474274
Sherin Thomas <sherint275@gmail.com>
2025-07-21 22:14:13 +0530

    client changes

--- src/db/client.ts
+++ src/db/client.ts
@@ -3,7 +3,4 @@ import { PrismaClient } from "../generated/prisma";
 const prisma = new PrismaClient();
 
 export { prisma };
-<<<<<<< HEAD
 
-=======
->>>>>>> 72d34082b0d53e4409b1c9e31ec1e620b1dd4189
