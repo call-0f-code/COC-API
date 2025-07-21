c677838a389e372021090b2c41d7478e345b2b7d
mrforgotten <harishnarote7@gmail.com>
2025-07-21 15:30:29 +0530

    Refactor question service methods to remove try-catch blocks

--- src/services/question.service.ts
+++ src/services/question.service.ts
@@ -1,60 +1,56 @@
 import { prisma } from "../db/client"
-import { Difficulty} from "../generated/prisma";
+import { Difficulty } from "../generated/prisma";
 
 
-export const getQuestionByTopicId = async(TopicId:number) =>{
-    try{
-        const questions = await prisma.question.findMany({
-            where: {topicId:TopicId}
-        })
-        return questions
-    }catch(error){
-        throw Error(error as string);
-    }
-}
+export const getQuestionByTopicId = async (TopicId: number) => {
 
-export const getQuestionByQuestionId = async(questionId:number) =>{
-    
-    return await prisma.question.findUnique({
-        where: {id:questionId}
+    return await prisma.question.findMany({
+        where: { topicId: TopicId }
     })
-        
-    
-    
-}
-export const addQuestionByTopicId = async(adminId:string,TopicId:number,link:string,difficulty:Difficulty,questionName:string)=>{
-    
-        return await prisma.question.create({
-            data: {
-                questionName,
-                difficulty,
-                link,
-                topicId:TopicId,
-                createdById:adminId,
-                updatedById:adminId
-            }
-        })
-        
+
+
 }
 
-export const updateQuestion = async(questionData:questionData,questionId:number)=>{
-   
-        return await prisma.question.update({
-            where: {id:questionId},
-            data: questionData
-        })
-    
+export const getQuestionByQuestionId = async (questionId: number) => {
+
+    return await prisma.question.findUnique({
+        where: { id: questionId }
+    })
+
+
+
+}
+export const addQuestionByTopicId = async (adminId: string, TopicId: number, link: string, difficulty: Difficulty, questionName: string) => {
+
+    return await prisma.question.create({
+        data: {
+            questionName,
+            difficulty,
+            link,
+            topicId: TopicId,
+            createdById: adminId,
+            updatedById: adminId
+        }
+    })
+
 }
 
-export const deleteQuestion = async(questionId:number)=>{
-    try{
-        const question = await prisma.question.delete({
-            where : {id:questionId}
-        })
-        return question
-    }catch(error){
-        throw new Error(error as string);
-    }
+export const updateQuestion = async (questionData: questionData, questionId: number) => {
+
+    return await prisma.question.update({
+        where: { id: questionId },
+        data: questionData
+    })
+
+}
+
+export const deleteQuestion = async (questionId: number) => {
+
+    return await prisma.question.delete({
+        where: { id: questionId }
+    })
+
+
 }
 
 
